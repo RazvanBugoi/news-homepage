868d0d9556fb5f0e08e4250a4b423536c869bb98
Razvan <razvanbugoi@gmail.com>
2023-05-20 16:43:35 +0100

    Use JavaScript to dynamically populate card contents

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Hamburger Menu
+
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
@@ -16,3 +18,69 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+
+
+// Data to populate card contents
+
+// Define an array of card data
+const cardData = [
+    {
+        img: "./assets/images/image-retro-pcs.jpg",
+        number: "01",
+        title: "Reviving Retro PCs",
+        content: "What happens when old PCs are given modern upgrades?",
+    },
+    {
+        img: "./assets/images/image-top-laptops.jpg",
+        number: "02",
+        title: "Top 10 Laptops of 2022",
+        content: "Our best picks for various needs and budgets.",
+    },
+    {
+        img: "./assets/images/image-gaming-growth.jpg",
+        number: "03",
+        title: "The Growth of Gaming",
+        content: "How the pandemic has sparked fresh opportunities.",
+    }
+];
+
+// Function to create a card component with populated content
+function createCard(card) {
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card");
+
+    const imgElement = document.createElement("img");
+    imgElement.src = card.img;
+    cardElement.appendChild(imgElement);
+
+    const contentElement = document.createElement("div");
+    contentElement.classList.add("content");
+
+    const numberElement = document.createElement("p");
+    numberElement.classList.add("post-number");
+    numberElement.textContent = card.number;
+    contentElement.appendChild(numberElement);
+
+    const titleElement = document.createElement("h4");
+    titleElement.classList.add("post-title");
+    titleElement.textContent = card.title;
+    contentElement.appendChild(titleElement);
+
+    const contentTextElement = document.createElement("p");
+    contentTextElement.classList.add("post-description");
+    contentTextElement.textContent = card.content;
+    contentElement.appendChild(contentTextElement);
+
+    cardElement.appendChild(contentElement);
+
+    return cardElement;
+}
+
+// Get the card section element
+const cardSection = document.querySelector(".cards");
+
+// Populate the cards dynamically
+cardData.forEach((card) => {
+    const cardElement = createCard(card);
+    cardSection.appendChild(cardElement);
+})
